19fc53c054e29e7d03ed7283d6938039b3dd1677
is0ly <ljaosipov@gmail.com>
2020-01-27 19:21:14 +0300

    added calc

--- js/app.js
+++ js/app.js
@@ -241,4 +241,56 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
+
+    /// Relax Calculator
+
+    // Calc
+
+    let persons = document.querySelectorAll(".counter-block-input")[0],
+        restDays = document.querySelectorAll(".counter-block-input")[1],
+        place = document.getElementById("select"),
+        totalValue = document.getElementById("total"),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener("change", function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == "") {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+
+        if (persons.value == 0) {
+            totalValue.innerHTML = 0;
+        }
+    });
+
+    restDays.addEventListener("change", function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == "") {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+        if (restDays.value == 0) {
+            totalValue.innerHTML = 0;
+        }
+    });
+
+    place.addEventListener("change", function() {
+        if (restDays.value == "" || persons.value == "") {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 });
